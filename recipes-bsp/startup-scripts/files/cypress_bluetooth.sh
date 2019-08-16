9fd90705208c0387d3f780ee9fa05e0fc9a33117
David Frey <dfrey@sierrawireless.com>
2019-08-15 21:01:52 -0700

    set smack permissions to enable SensorTag app

--- recipes-bsp/startup-scripts/files/cypress_bluetooth.sh
+++ recipes-bsp/startup-scripts/files/cypress_bluetooth.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
+attach_cypress_bt()
+{
+    btattach -P h4 -B /dev/ttyUSB0 &
+}
+
+# bluetoothSensorTag needs to be able to open a socket to dbus-daemon and vice
+# versa. Since dbus-daemon runs under smack label "_" it's necessary to allow
+# all processes running with label _ to access the bluetoothSensorTag
+# application and necessary for bluetoothSensorTag application to be able to
+# access all processes running as label _. It is especially unfortunate that
+# this has to be customized for each bluetooth Legato app.
+sensortag_smack_workaround()
+{
+    echo "_ app.bluetoothSensorTag -w----" > /sys/fs/smackfs/load2
+    echo "app.bluetoothSensorTag _ -w----" > /sys/fs/smackfs/load2
+}
+
+do_start()
+{
+    attach_cypress_bt
+    sensortag_smack_workaround
+}
+
 case $1 in
     start)
-        btattach -P h4 -B /dev/ttyUSB0 &
+        do_start
     ;;
+
     *)
         echo "Operation $1 - not supported"
         exit 1
